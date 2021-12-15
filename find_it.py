9c51f2a7c9b484a26da46cbb21578edcab5b3819
gaolin1 <jackma84@gmail.com>
2021-12-14 21:17:28 -0500

    modified for gram positive and new sample output

--- find_it.py
+++ find_it.py
@@ -8,16 +8,29 @@ import xlsxwriter
 
 def main():
     df = import_combined()
-    df = pivot(df)
-    df_removed = remove_first_specimen(df)
+    df_complete = pivot(df)
+    df_removed = remove_first_specimen(df_complete)
     export_df(df, df_removed)
 
 
-def remove_first_specimen(df):
-    print(df)
-    df = df.sort_values(by=["Received", "Organism"])
-    df_removed = df.drop_duplicates(subset=["Patient Name", "Organism"])
-    print(df_removed)
+def remove_first_specimen(df_complete):
+    #print(df_complete)
+    df_staphyl = df_complete[df_complete["Organism"].str.contains("Staphylococcus aureus", na=False)]
+    df_staphyl_sorted = df_staphyl.sort_values(by="Received")
+    df_staphyl_removed = df_staphyl_sorted.drop_duplicates(subset=["Patient Name"])
+    #print(df_staphyl_removed)
+    df_entero = df_complete[df_complete["Organism"].str.contains("Enterococcus", na=False)]
+    #print(df_entero)
+    df_entero = df_entero[~df_entero["Organism"].str.contains("gallinarum", na=False)]
+    #print(df_entero)
+    df_entero_removed = df_entero[~df_entero["Organism"].str.contains("casseliflavus", na=False)]
+    df_entero_sorted = df_entero_removed.sort_values(by="Received")
+    df_entero_ready = df_entero_sorted.drop_duplicates(subset="Patient Name")
+    #print(df_entero_removed)
+    df_removed = df_staphyl_removed.append(df_entero_ready)
+    #df_removed = df_removed.sort_values(by=["Received", "Organism"])
+    #df_removed = df_removed.drop_duplicates(subset=["Patient Name", "Organism"])
+    #print(df_removed)
     return df_removed
 
 
@@ -57,6 +70,7 @@ def import_combined():
         #df.drop_duplicates()
     df = df.set_index("Received")
     df = df.reset_index()
+    #to ensure if ' ' will not cause loss of data
     df["Organism"] = df["Organism"].fillna("dummy")
     df["Antibiotic Interpretation"] = df["Antibiotic Interpretation"].fillna("dummy")
     df["Resulted"] = df["Resulted"].fillna("dummy")
@@ -82,7 +96,7 @@ def export_df(df1, df2):
     write_path = input("Enter location for output xlsx file: ")
     write = pd.ExcelWriter(write_path, engine='xlsxwriter')
     df1.to_excel(write, "Complete")
-    df2.to_excel(write, "First Specimen Only")
+    df2.to_excel(write, "First Specimen (Gram Positive)")
     write.save()
 
 
