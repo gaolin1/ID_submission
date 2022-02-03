6d9b0c5493ebf032a0aa907bf65156bf96722e86
gaolin1 <jackma84@gmail.com>
2022-02-02 22:55:13 -0500

    added non-forced function for report type

--- dot_visual.py
+++ dot_visual.py
@@ -5,15 +5,38 @@ import sys
 from streamlit import cli as stcli
 
 def main():
-    main_type = input("Please indicate type of report (DDD/DOT): ")
-    while main_type not in ("DDD", "DOT"):
-        main_type = input("Please enter either DDD or DOT: ")
-    if main_type == "DDD":
-        df_ddd_department = import_df("DDD per Department")
-        df_ddd_department = import_processed(df_ddd_department, "DDD Department")
-        df_ddd_location = import_df("DDD per Location")
-        df_ddd_location = import_processed(df_ddd_location, "DDD Location")
-        export_df(df_ddd_department, "DDD Department", df_ddd_location, "DDD Location")
+    report_input = input("Process Epic Report? (Y/N): ")
+    while report_input not in ("Y", "N"):
+        report_input = input("Please enter either Y or N: ")
+    if report_input == "Y":
+        main_type = input("Please indicate type of report (DDD/DOT) to be processed: ")
+        while main_type not in ("DDD", "DOT"):
+            main_type = input("Please enter either DDD or DOT: ")
+        sub_type = int(input("Please choose one of the following (1/2/3):\n1. Location\n2. Department\n3. Both\n"))
+        while sub_type not in (1,2,3):
+            sub_type = int(input("Please enter 1, 2 or 3: "))
+        if sub_type == 1:
+            sub_type = "Location"
+            import_type_location = main_type + " per " + sub_type
+            df_type_location = import_df(import_type_location)
+            df_type_location = import_processed(df_type_location, main_type, sub_type)
+            export_one_df(df_type_location, import_type_location)
+        elif sub_type == 2:
+            sub_type = "Department"
+            import_type_department = main_type + " per Department"
+            df_type_department = import_df(import_type_department)
+            df_type_department = import_processed(df_type_department, main_type, sub_type)
+            export_one_df(df_type_department, import_type_department)  
+        else:
+            sub_type = "Location"
+            import_type_location = main_type + " per Location"
+            df_type_location = import_df(import_type_location)
+            df_type_location = import_processed(df_type_location, main_type, sub_type)
+            sub_type = "Department"
+            import_type_department = main_type + " per Department"
+            df_type_department = import_df(import_type_department)
+            df_type_department = import_processed(df_type_department, main_type, sub_type)
+            export_two_df(df_type_location, import_type_location, df_type_department, import_type_department)
     else:
         pass
     launch_input = input("Launch dashboard app? (Y/N): ")
@@ -25,8 +48,8 @@ def main():
     else:
         quit()
 
-def import_processed(df, type):
-    df[type + " Per 1000 Patients"] = df["Per 1000 Patients"]
+def import_processed(df, main_type, sub_type):
+    df[main_type + " " + sub_type + " Per 1000 Patients"] = df["Per 1000 Patients"]
     df = df.drop(columns=["Per 1000 Patients"])
     df["GROUPER_NAME"] = df["GROUPER_NAME"].str.replace("ERX CONCEPT", "")
     return df
@@ -42,7 +65,13 @@ def import_df(type):
     df = pd.read_excel(decrypted)
     return df
 
-def export_df(df1, type1, df2, type2):
+def export_one_df(df1, type1):
+    write_path = input("Enter location for output xlsx file: ")
+    write = pd.ExcelWriter(write_path, engine='xlsxwriter')
+    df1.to_excel(write, type1, index=False)
+    write.save()
+
+def export_two_df(df1, type1, df2, type2):
     write_path = input("Enter location for output xlsx file: ")
     write = pd.ExcelWriter(write_path, engine='xlsxwriter')
     df1.to_excel(write, type1, index=False)
--- dot_visual_st.py
+++ dot_visual_st.py
@@ -1,3 +1,4 @@
+from typing import Container
 import pandas as pd
 from pandas.io import excel
 import streamlit as st
@@ -7,11 +8,21 @@ def main():
     try:
         excel = st.file_uploader("Upload Epic export", type=["xlsx"])
         type = st.radio("Select either DDD (Defined Daily Dose) or DOT (Days Of Therapy)",["DDD", "DOT"])
-        type_dep = type + " Department"
-        type_loc = type + " Location"
-        df_dep = pd.read_excel(excel, type_dep)
-        df_loc = pd.read_excel(excel, type_loc)
-        grab_drug(df_dep, df_loc, type)
+        report_type = st.radio("Select type of uploaded report", ["Location", "Department", "Both"])
+        if report_type == "Both":
+            type_dep = type + " per Department"
+            type_loc = type + " per Location"
+            df_dep = pd.read_excel(excel, type_dep)
+            df_loc = pd.read_excel(excel, type_loc)
+            grab_drug_both(df_dep, df_loc, type)
+        elif report_type == "Location":
+            type_loc = type + " per Location"
+            df_loc = pd.read_excel(excel, type_loc)
+            grab_drug_one(df_loc, type, type_loc)
+        else:
+            type_dep = type + " per Department"
+            df_loc = pd.read_excel(excel, type_dep)
+            grab_drug_one(df_loc, type, type_dep)         
     except ValueError or TypeError or AttributeError:
         pass
 
@@ -26,16 +37,20 @@ def prepare_loc(df, drug_list, loc_list):
 def make_chart(data, dep_or_loc, ddd_or_dot, type):
     data = data.reset_index()
     value_type = ddd_or_dot + " " + dep_or_loc + " Per 1000 Patients"
+    base = alt.Chart(data).properties(height=450,width=800)
     chart = (
-        alt.Chart(data)
-        .mark_line(opacity=0.4)
+        base.mark_line(opacity=0.4)
         .encode(
-            x="MONTH_BEGIN_DT:T",
-            y=alt.Y(value_type + ":Q",stack=None, aggregate="sum"),
+            x=alt.X("month(MONTH_BEGIN_DT):T", title = "month"),
+            y=alt.Y(value_type + ":Q",stack=None, aggregate="sum", title = ddd_or_dot + " per 1000 patient days "),
             color=type+":N"
         )
         .interactive()
         )
+    rule = base.mark_rule(color='red', opacity=0.8, strokeDash=[1,1]).encode(
+        y='average(' + value_type + '):Q'
+        )
+    chart = chart + rule
     return chart
 
 def prepare_df(df):
@@ -64,7 +79,7 @@ def checkbox(df, drug, type):
     except KeyError:
         pass
 
-def grab_drug(df, df2, type):
+def grab_drug_both(df, df2, type):
     df = df.set_index("GROUPER_NAME")
     drug_list = df.index.unique().tolist()
     container = st.container()
@@ -83,11 +98,38 @@ def grab_drug(df, df2, type):
     df_loc_dep, dep_list = checkbox(df_loc, drug_selected, "DEPARTMENT_NAME")
     df_loc = prepare_loc(df2, drug_selected, loc_list)
     dep_chart = make_chart(df_loc_dep, "Department", type, "DEPARTMENT_NAME")
-    loc_chart = make_chart(df_loc, "Location", type,"LOC_NAME")
+    loc_chart = make_chart(df_loc, "Location", type, "LOC_NAME")
     show_df(df_loc)
     show_df(df_loc_dep)
     st.altair_chart(loc_chart, use_container_width=True)
     st.altair_chart(dep_chart, use_container_width=True)
+
+def grab_drug_one(df, ddd_or_dot, type):
+    df = df.set_index("GROUPER_NAME")
+    drug_list = df.index.unique().tolist()
+    container = st.container()
+    all = st.checkbox("Select all medication groupers")
+    if all:
+        drug_selected = container.multiselect("Choose grouper", options=drug_list, default=drug_list, key="4")
+        if not drug_selected:
+            container.error("Please select at least one grouper or use select all")
+    else:
+        drug_selected = container.multiselect("Choose grouper", options=drug_list, key="1")
+        if not drug_selected:
+            container.error("Please select at least one grouper")
+    #st.text(drug_selected)
+    if "Location" in type:
+        df_loc, loc_list = checkbox(df, drug_selected, "LOC_NAME")
+        df_loc = prepare_df(df_loc)
+        loc_chart = make_chart(df_loc, "Location", ddd_or_dot, "LOC_NAME")
+        show_df(df_loc)
+        st.altair_chart(loc_chart, use_container_width=True)
+    else:
+        df_dep, dep_list = checkbox(df, drug_selected, "DEPARTMENT_NAME")
+        df_dep = prepare_df(df_dep)
+        dep_chart = make_chart(df_dep, "Department", ddd_or_dot, "DEPARTMENT_NAME")
+        show_df(df_dep)
+        st.altair_chart(dep_chart, use_container_width=True)
 
 def show_df(df):
     df = df.astype(str)
